f68e8642759e3caa82e3ef3f23d1b4c9f8add2ab
zuhri41code <faizidn4@gmail.com>
2024-10-31 09:22:28 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_crud/screens/addUser.dart';
+import 'package:sqflite_crud/screens/view_user.dart';
+import 'package:sqflite_common/sqflite.dart';
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ViewUserPage(),
+    );
+  }
+}
